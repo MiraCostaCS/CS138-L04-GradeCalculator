0512d6ac7ee84516b9f9c4490f4d9434c142a56b
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2025-05-14 12:16:20 -0700

    added readme and starter code

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+COLLABORATOR(S): name(s) of anyone who helped you
+'''
+""" 
+This program can be used to determine final course grade or to find the expected number of assignments for a given week of the semester.
+"""
+
+##### FUNCTION DEFINITIONS #####
+
+
+
+##### MAIN PROGRAM #####
